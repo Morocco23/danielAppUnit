2523ed8a38fffbd691644ed1e2af98a535067b24
Daniel Eniola Favour <eniolad206@gmail.com>
2025-05-16 00:12:00 +0100

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+    const menuIcon = document.getElementById("menu-icon");
+    // Change icon to X when menu is open
+    if (navLinks.style.display === 'flex') {
+        menuIcon.style.transform = "rotate(-90deg)";
+    } else {
+        menuIcon.style.transform = "rotate(0deg)";
+    }
+}
